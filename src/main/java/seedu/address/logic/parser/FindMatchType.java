f5953c9a2a0fe6640562ec59937453359520fc96
Whimsyturtle <19924575+Whimsyturtle@users.noreply.github.com>
2026-03-17 21:37:45 +0800

    Implement new find command parser logic

---
+++ src/main/java/seedu/address/logic/parser/FindMatchType.java
@@ -0,0 +1,26 @@
+package seedu.address.logic.parser;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+/**
+ * Supported match types for the find command.
+ */
+public enum FindMatchType {
+    KEYWORD("keyword");
+
+    private final String token;
+
+    FindMatchType(String token) {
+        this.token = token;
+    }
+
+    /**
+     * Returns the {@code FindMatchType} that corresponds to the given {@code token}, if any.
+     */
+    public static Optional<FindMatchType> fromToken(String token) {
+        return Arrays.stream(FindMatchType.values())
+                .filter(matchType -> matchType.token.equals(token))
+                .findFirst();
+    }
+}
--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MATCH_TYPE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
@@ -9,7 +10,7 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
-import seedu.address.model.person.PersonContainsKeywordsPredicate;
+import seedu.address.model.person.predicate.PersonContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
 
@@ -26,9 +27,30 @@ public class FindCommandParserTest {
         FindCommand expectedFindCommand =
                 new FindCommand(new PersonContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
         assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
+        assertParseSuccess(parser, PREFIX_MATCH_TYPE + "keyword Alice Bob", expectedFindCommand);
 
         // multiple whitespaces between keywords
         assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+        assertParseSuccess(parser, " \n " + PREFIX_MATCH_TYPE + "keyword \n \t Alice  \t Bob  \t",
+                expectedFindCommand);
+    }
+
+    @Test
+    public void parse_matchTypeNotFirstToken_throwsParseException() {
+        assertParseFailure(parser, "Alice " + PREFIX_MATCH_TYPE + "keyword Bob",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_unsupportedMatchType_throwsParseException() {
+        assertParseFailure(parser, PREFIX_MATCH_TYPE + "regex Alice Bob",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_missingKeywords_throwsParseException() {
+        assertParseFailure(parser, PREFIX_MATCH_TYPE + "keyword",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
     }
 
 }
