ec8b066230337abc899095bcbbe47eb26f43030f
Whimsyturtle <19924575+Whimsyturtle@users.noreply.github.com>
2026-04-02 07:30:49 +0800

    Suppress checkstyle

--- src/main/java/seedu/address/logic/parser/PersonContainsFieldsPredicateFactory.java
+++ src/main/java/seedu/address/logic/parser/PersonContainsFieldsPredicateFactory.java
@@ -20,10 +20,13 @@ public final class PersonContainsFieldsPredicateFactory {
      * Returns a {@code PersonContainsFieldsPredicate} for the given match type and keywords.
      */
     public static PersonContainsFieldsPredicate createPredicate(FindMatchType matchType, List<String> keywords) {
+        // Indented case blocks in lambda-style switch statements are allowed
+        // CHECKSTYLE.OFF: Indentation
         return switch (matchType) {
-            case KEYWORD -> new PersonContainsKeywordsPredicate(keywords);
-            case SUBSTRING -> new PersonContainsSubstringsPredicate(keywords);
-            case FUZZY -> new PersonContainsFuzzyKeywordsPredicate(keywords);
+        case KEYWORD -> new PersonContainsKeywordsPredicate(keywords);
+        case SUBSTRING -> new PersonContainsSubstringsPredicate(keywords);
+        case FUZZY -> new PersonContainsFuzzyKeywordsPredicate(keywords);
         };
+        // CHECKSTYLE.ON: Indentation
     }
 }
